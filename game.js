fc76df70e06fb34d9ca50d92c82d3f0106218200
Lobodass <158287475+Lobodass@users.noreply.github.com>
2025-02-27 19:16:23 +0200

    ааа

---
+++ game.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hiddenText = document.getElementById("hiddenText");
+
+    function checkScroll() {
+      const position = hiddenText.getBoundingClientRect().top;
+      const screenHeight = window.innerHeight;
+
+      if (position < screenHeight - 50) {
+        hiddenText.classList.add("show");
+        window.removeEventListener("scroll", checkScroll); // Вимикаємо, щоб не спрацьовувало повторно
+      }
+    }
+
+    window.addEventListener("scroll", checkScroll);
+  });
+
+    
