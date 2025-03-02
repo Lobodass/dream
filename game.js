3da1c605379db7ad77db11fd47e218370fcf1a9f
Lobodass <158287475+Lobodass@users.noreply.github.com>
2025-03-02 22:02:59 +0200

    ewfg

--- game.js
+++ game.js
@@ -13,5 +13,19 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("scroll", checkScroll);
     checkScroll(); // Викликати одразу, щоб перевірити початковий стан
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const elements = document.querySelectorAll(".fade-effect");
 
+    function checkScroll() {
+        elements.forEach((el) => {
+            let rect = el.getBoundingClientRect();
+            if (rect.top < window.innerHeight - 50) {
+                el.classList.add("show");
+            }
+        });
+    }
+
+    window.addEventListener("scroll", checkScroll);
+    checkScroll(); // Викликати одразу, щоб перевірити початковий стан
+});
     
